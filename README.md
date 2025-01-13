ee9219dfb68bb77de115af44b6d4e0fee29f4202
YVNNVLEX <nguessankouakouyannalex@gmail.com>
2025-01-13 10:27:32 +0000

    core exercise done

---
+++ README.md
@@ -0,0 +1,20 @@
+# Roadmap.sh Github Activities CLI
+
+According to my training on roadmap.sh site, I build a simple command line interface (CLI) to fetch the recent activity of a GitHub user and display it in the terminal. This project will help me to practice my Python programming skills, including working with APIs, handling JSON data, and building a simple CLI application.  
+
+Usage of this CLI is simple:
+The syntax is like: user **Github user valid name**
+
+Example: user ln-dev7
+Output:
+User has no recent issue
+User ln-dev7 pushed 23 commits to ln-dev7/pqoqubbw-icons
+User has no recent PullRequest
+started rorkai/21st
+
+For now it's just a training app, I will improve the features to filtering and many funny feature.
+
+Thanks for reading me, and hope I will receive many stars and advice for my codebase.
+😘
+
+https://roadmap.sh/projects/github-user-activity
--- main.py
+++ main.py
@@ -9,49 +9,75 @@ class github_activity(cmd.Cmd):
             url = f'https://api.github.com/users/{arg}/events'
             getting_content = requests.get(url)
             contents = getting_content.json()
-            allPushed = []
-            allIssues = []
-            allPR = []
-            allStarred = []
-            for event in contents:
-                match event['type']:
-                    case "PushEvent":
-                        allPushed.append(event)
-                    case "WatchEvent":
-                        allStarred.append(event)
-                    case "IssuesEvent":
-                        allIssues.append(event)
-                    case "PullRequestsEvent":
-                        allPR.append(event)
-                    case _:
-                        continue
-            if len(allIssues) == 0 or len(allPR) == 0 or len(allStarred) == 0 or len(allPushed) == 0 :
-                if len(allIssues) ==0:
-                    pass
+            if len(contents) != 0 and getting_content.status_code == 200:
+                allPushed = []
+                allIssues = []
+                allPR = []
+                allStarred = []
+                firstPR = None
+                firstIssue = None
+                firstPushed = None
+                firstStarred = None
+                for event in contents:
+                    match event['type']:
+                        case "PushEvent":
+                            allPushed.append(event)
+                        case "WatchEvent":
+                            allStarred.append(event)
+                        case "IssuesEvent":
+                            allIssues.append(event)
+                        case "PullRequestsEvent":
+                            allPR.append(event)
+                        case _:
+                            continue
+                if len(allIssues) == 0 or len(allPR) == 0 or len(allStarred) == 0 or len(allPushed) == 0 :
+                    if len(allIssues) ==0:
+                        pass
+                    else:
+                        firstIssue = allIssues[0]
+                        actionIssues = firstIssue['payload']['action']
+                        repoIssues = firstIssue['repo']['name']
+                    if len(allPR) ==0:
+                        pass
+                    else:
+                        firstPR = allPR[0]
+                        actionPR = firstPR['payload']['action']
+                        repoPR = firstPR['repo']['name']
+                    if len(allStarred) ==0:
+                        pass
+                    else:
+                        firstStarred = allStarred[0]
+                        actionStarred = firstStarred['payload']['action']
+                        repoStarred = firstStarred['repo']['name']
+                    if len(allPushed) ==0:
+                        pass
+                    else:
+                        firstPushed = allPushed[0]
+                        commitSize = firstPushed['payload']['size']
+                        repoPushed = firstPushed['repo']['name']
+                
+                if firstIssue:
+                    print (f"{actionIssues} a issue in {repoIssues}")
                 else:
-                    firstIssue = allIssues[0]
-                    actionIssues = firstIssue['payload']['action']
-                    repoIssues = firstIssue['repo']['name']
-                if len(allPR) ==0:
-                    pass
+                    print (f"User has no recent issue")
+                
+                if firstPushed:
+                    print(f"User {arg} pushed {commitSize} commits to {repoPushed}")
                 else:
-                    firstPR = allIssues[0]
-                    actionPR = firstPR['payload']['action']
-                    repoPR = firstPR['repo']['name']
-                if len(allStarred) ==0:
-                    pass
-                else:
-                    firstStarred = allStarred[0]
-                    actionStarred = firstStarred['payload']['action']
-                    repoStarred = firstStarred['repo']['name']
-                if len(allPushed) ==0:
-                    pass
-                else:
-                    firstPushed = allPushed[0]
-                    commitSize = firstPushed['payload']['size']
-                    repoPushed = firstPushed['repo']['name']
+                    print('User not pushed on the last days')
                     
-            
+                if firstPR:
+                    print (f"{actionPR} a PullRequest in {repoPR}")
+                else:
+                    print (f"User has no recent PullRequest")
+                
+                if firstStarred:
+                    print (f"{actionStarred} {repoStarred}")
+                else:
+                    print (f"User has not starred a repo on the last day")
+            else:
+                print(f"User {arg} has not a Github Account")    
+                
     def do_exit(self,line):
         return True
 
